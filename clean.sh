eafb0b39ef3829d62e923ba091c65eafc97a65ea
Tomoyuki Sakurai <y@trombik.org>
2020-06-19 21:00:17 +0700

    initial implementation

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -f avr lib/gcc/avr
+rm -rf lib
+rm -f package.json
---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+: ${AVR_DIR:="/usr/local/avr"}
+: ${AVR_GCC_DIR:="/usr/local/lib/gcc/avr"}
+
+VERSION="1.70300.191015"
+DESC="toolchain-atmelavr"
+REPO_NAME="platformio-freebsd-${DESC}"
+REPO_OWNER="trombik"
+
+ARCH=`uname -p`
+OS=`uname -s | tr '[[:upper:]]' '[[:lower:]]'`
+
+if [ ! -z $1 ]; then
+    VERSION="$1"
+fi
+
+ln -s -f ${AVR_DIR} .
+mkdir -p lib/gcc
+ln -s -f ${AVR_GCC_DIR} lib/gcc/avr
+
+cat <<__EOF__ >package.json
+{
+    "description": "${DESC}",
+    "name": "${DESC}",
+    "system": [
+        "${OS}_${ARCH}"
+    ],
+    "url": "https://github.com/${REPO_OWNER}/${REPO_NAME}",
+    "version": "${VERSION}"
+}
+__EOF__
